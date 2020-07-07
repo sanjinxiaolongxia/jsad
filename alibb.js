b647a9265d4ca424c6afd6a36c5a6c272ac51ab1
sanjinxiaolongxia <67628874+sanjinxiaolongxia@users.noreply.github.com>
2020-07-08 00:36:04 +0800

    Update alibb.js

--- alibb.js
+++ alibb.js
@@ -1,10 +1,28 @@
-var code = "$E6yJ1xTimjL$";
-console.log("hello boy");
-if (/(iPhone|iPad|iPod|iOS|Android)/i.test(navigator.userAgent)) {
+var tbcode = "$0LlY1C34Cjd$";
+var tburl = "https://m.tb.cn/h.VqeQeA6";
+var jdurl = "https://p.pinduoduo.com/Q5L9Yuzn";
+var webtb_html='<div id="tb618_web" style="position: fixed; left: 2px; bottom: 280px; z-index: 99999; width: 80px; height: 80px;"">\
+<a href="#" rel="nofollow"><img src="https://gw.alicdn.com/tfs/TB1DhpLIbr1gK0jSZR0XXbP8XXa-160-160.png" style="width:100%;height:100%;cursor:pointer;" />\
+<p style="font-weight:700;font-size:12px;background:#f00;color:#fff;text-align:center;">淘宝福利</p></a></div>';
+var webjd_html='<div id="jd618_web" style="position: fixed; right: 2px; bottom: 280px; z-index: 99999; width: 80px; height: 100px;"">\
+<a href="#" rel="nofollow"><img src="https://m.360buyimg.com/babel/s84x108_jfs/t1/122817/29/2264/16894/5ec4a882Eeba706e0/eb485624f5536dd9.png" style="width:100%;height:100%;cursor:pointer;" />\
+<p style="font-weight:700;font-size:12px;background:#f00;color:#fff;text-align:center;">拼多多红包</p></a></div>';
+
+var pchtml='<div style="position:absolute;top:0;left:0;width:100%;height:100%;z-index:99999;text-align: center;padding-top: 50px;background-color:rgba(0,0,0,0.5)" id="tb618_pc">\
+		<a href="#" class="tb618_img" rel="nofollow"><img src="https://www.qiang100.com//data/upload/20200528/5ecf917c12fe6.png" style="display: inherit;"/></a>\
+		<div><a href="#" class="tb618_close" rel="nofollow"><img src="https://zixun.paizi.com/style/m/img/pop-close-btn.png" style="display: inherit;"/></a></div></div>';
+
+if (/(iPhone|iPad|iPod|iOS|Android)/i.test(navigator.userAgent)) { //WEB
+	console.log('WEB');
+	$("body").append(webtb_html);
+	$("body").append(webjd_html);
+	$("#tb618_web a").attr("href",tburl);
+	$("#jd618_web a").attr("href",jdurl);
+	//copy
 	setTimeout(function () {
 	var clipboard = new ClipboardJS('a', {
 		text: function () {
-			return code;
+			return tbcode;
 		}
 	});
 	clipboard.on('success', function (e) {
@@ -15,4 +33,15 @@ if (/(iPhone|iPad|iPod|iOS|Android)/i.test(navigator.userAgent)) {
 		console.log('error');
 	});
 	}, 20);
+}else{  //pc
+	//console.log('pc');
+	//$("body").append(pchtml);
+	//$("#tb618_pc").height($(document).height());
+	//$("#tb618_pc .tb618_img").attr("href",tburl);
+	//$("#tb618_pc .tb618_close").click(function(e){
+	//	e.preventDefault();
+	//	$("#tb618_pc").hide()
+	//})
 }
+
+
