d8fa642f89dfaf093d81ec0b8c78aeefc7a3f283
sanjinxiaolongxia <67628874+sanjinxiaolongxia@users.noreply.github.com>
2020-07-08 13:32:54 +0800

    Update alibb.js

--- alibb.js
+++ alibb.js
@@ -1,12 +1,12 @@
 var tbcode = "$0LlY1C34Cjd$";
 var tburl = "https://m.tb.cn/h.VqeQeA6";
-var jdurl = "https://p.pinduoduo.com/Q5L9Yuzn";
+var jdurl = "https://p.pinduoduo.com/btC9NtxU";
 var webtb_html='<div id="tb618_web" style="position: fixed; left: 2px; bottom: 280px; z-index: 99999; width: 80px; height: 80px;"">\
 <a href="#" rel="nofollow"><img src="https://gw.alicdn.com/tfs/TB1DhpLIbr1gK0jSZR0XXbP8XXa-160-160.png" style="width:100%;height:100%;cursor:pointer;" />\
 <p style="font-weight:700;font-size:12px;background:#f00;color:#fff;text-align:center;">淘宝福利</p></a></div>';
-var webjd_html='<div id="jd618_web" style="position: fixed; right: 2px; bottom: 280px; z-index: 99999; width: 80px; height: 100px;"">\
-<a href="#" rel="nofollow"><img src="https://m.360buyimg.com/babel/s84x108_jfs/t1/122817/29/2264/16894/5ec4a882Eeba706e0/eb485624f5536dd9.png" style="width:100%;height:100%;cursor:pointer;" />\
-<p style="font-weight:700;font-size:12px;background:#f00;color:#fff;text-align:center;">拼多多红包</p></a></div>';
+var webjd_html='<div id="jd618_web" style="position: fixed; right: 2px; bottom: 280px; z-index: 99999; width: 80px; height: 80px;"">\
+<a href="#" rel="nofollow"><img src="https://s1.ax1x.com/2020/07/08/UEOxtx.png" style="width:100%;height:100%;cursor:pointer;" />\
+<p style="font-weight:700;font-size:12px;background:#E02E24;color:#fff;text-align:center;">拼多多红包</p></a></div>';
 
 var pchtml='<div style="position:absolute;top:0;left:0;width:100%;height:100%;z-index:99999;text-align: center;padding-top: 50px;background-color:rgba(0,0,0,0.5)" id="tb618_pc">\
 		<a href="#" class="tb618_img" rel="nofollow"><img src="https://www.qiang100.com//data/upload/20200528/5ecf917c12fe6.png" style="display: inherit;"/></a>\
