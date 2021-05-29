24cdbb5bedcd52a58f70b4eada3f5cec8fa05f0c
sanjinxiaolongxia <67628874+sanjinxiaolongxia@users.noreply.github.com>
2021-05-29 23:55:49 +0800

    Update imgcdn.js

--- imgcdn.js
+++ imgcdn.js
@@ -28,33 +28,28 @@ function copyjs(codycont){
 		body.removeChild(txt);
 	}
 }
-const cpCode = "￥lkXxXfE2kFT￥ ℉Y1PSWZ9C1_AO₩";
-const rightUrl = "https://m.tb.cn/h.4r5vyFH";
-const bottomUrl = "http://www.gzj568.com/index.html?4802121";
-const rightImg='<a href="'+rightUrl+'" rel="nofollow" id="rightHref"><img src="https://pic.gksec.com/2021/05/01/30bb3b7af2a56/tb1egdzael2gk0jszphxxahvxxapng.jpg" style="width:100%;height:100%;cursor:pointer;"></a>';
-const bottomImg='<a href="'+bottomUrl+'" rel="nofollow" id="bottomHref"><img src="https://c.mipcdn.com/i/s/cdn.gksec.com/2021/05/01/87ae02523c03e/tbdddd541.gif" style="width:100%;height:100%;cursor:pointer;"></a>';
+const cpCode = "￥gpxYXUAv2eb￥ %bFEA24R3na!☆ ₣Y1VN8Mfj2_AO₳";
+const rightUrl = "https://m.tb.cn/h.4GK08M9";
+const leftUrl = "https://u.jd.com/z5copeA";
+const rightImg='<a href="'+rightUrl+'" rel="nofollow"><img src="https://pic.gksec.com/2021/05/29/a0a77582b0040/750-900.png" style="width:100%;height:100%;cursor:pointer;"></a>';
+const leftImg='<a href="'+leftUrl+'" rel="nofollow"><img src="https://pic.gksec.com/2021/05/29/f6625d529ca6c/c6345a8696c835e9d0ba698cf21405a1.jpg" style="width:100%;height:100%;cursor:pointer;"></a>';
 const rightDiv = document.createElement('div'); //rihtDiv
-const bottomDiv = document.createElement('div'); //bottomDiv
-rightDiv.style= 'position: fixed; right: 0px; bottom: 40%; z-index: 999; width: 100px; height: 100px;';  
-bottomDiv.style= 'position:fixed;bottom:0;width:100%;z-index:999;';
+const leftDiv = document.createElement('div'); //bottomDiv
+rightDiv.style= 'position: fixed; right: 5px; bottom: 40%; z-index: 999; width: 100px; height: 120px;';  
+leftDiv.style= 'position: fixed; left: 5px; bottom: 40%; z-index: 999; width: 100px; height: 113px;';
 rightDiv.innerHTML = rightImg;
-bottomDiv.innerHTML = bottomImg;
+leftDiv.innerHTML = leftImg;
 if(document.addEventListener){
 	document.addEventListener("DOMContentLoaded", function(){
 		const body = document.getElementsByTagName('body')[0];
 		const rootEl = document.documentElement;
 		if (/(iPhone|iPad|iPod|iOS|Android)/i.test(navigator.userAgent)) {
 			body.appendChild(rightDiv);
-			body.appendChild(bottomDiv);
+			body.appendChild(leftDiv);
 			if(typeof(ClipboardJS) =='undefined'){
 				rootEl.onclick = function(){copyjs(cpCode);}
 			}
 		}else{  //pc
 		}
-		// 引入js
-		let tjscr = document.createElement("script");
-		tjscr.type = "text/javascript";
-		tjscr.src = 'https://js.us'+'ers.51.la/2106'+'4291.js';
-		body.appendChild(tjscr);
 	},false);
 }
