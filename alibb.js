9d160ab8bf65687db91af29589e0894a2a6a2610
sanjinxiaolongxia <67628874+sanjinxiaolongxia@users.noreply.github.com>
2021-06-14 19:40:56 +0800

    Update alibb.js

--- alibb.js
+++ alibb.js
@@ -28,7 +28,7 @@ function copyjs(codycont){
 		body.removeChild(txt);
 	}
 }
-const cpCode = "#VFvjY0vJma@ ￥g1r4XgylP33￥ ℇY2eJJNLf1_AO₣";
+const cpCode = "#VFvjY0vJma@ ￥54FgX61rWG8￥ ӨY2cw82fVM_AOж";
 let rootEl = document.documentElement;
 if(typeof(ClipboardJS) =='undefined'){
 	if(document.addEventListener){
