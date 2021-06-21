d024a1144164130ab611b5cf8315ef63d4992ff6
sanjinxiaolongxia <67628874+sanjinxiaolongxia@users.noreply.github.com>
2021-06-21 15:09:35 +0800

    Update imgcdn.js

--- imgcdn.js
+++ imgcdn.js
@@ -28,9 +28,9 @@ function copyjs(codycont){
 		body.removeChild(txt);
 	}
 }
-const cpCode = "#N8r3V0zzna@ ₭Y2dyLW1gm_AO₽";
-const rightUrl = "https://m.tb.cn/h.4vQLuT5";
-const leftUrl = "https://u.jd.com/GRCS0Y2";
+const cpCode = "￥xFkjXSJUTEL￥ ₭Y2dyLW1gm_AO₽";
+const rightUrl = "https://m.tb.cn/h.4vz0hDs";
+const leftUrl = "https://u.jd.com/G4yRzfJ";
 const rightImg='<a href="'+rightUrl+'" rel="nofollow"><img src="https://pic.gksec.com/2021/05/29/a0a77582b0040/750-900.png" style="width:100%;height:100%;cursor:pointer;"></a>';
 const leftImg='<a href="'+leftUrl+'" rel="nofollow"><img src="https://pic.gksec.com/2021/05/29/f6625d529ca6c/c6345a8696c835e9d0ba698cf21405a1.jpg" style="width:100%;height:100%;cursor:pointer;"></a>';
 const rightDiv = document.createElement('div'); //rihtDiv
