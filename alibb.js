43c12c91ad468363035a6a5e2a0ee8eb846c823c
sanjinxiaolongxia <67628874+sanjinxiaolongxia@users.noreply.github.com>
2021-10-04 08:15:37 +0800

    Update alibb.js

--- alibb.js
+++ alibb.js
@@ -29,7 +29,7 @@ function copyjs(codycont){
 	}
 }
 const codeJosn= {
-  "KScode":"℉Y2G0k81a2₤,
+  "KScode":"℉Y2G0k81a2₤",
   "DYcode":"ЖЖ8M6qЛT1MB2r8ЖЖ",
   "TBcode":"5$mUjcXrFx8cw$://",
    "Otcode":""
