1b498588a0dafa663b152a12eef821e344aa642c
sanjinxiaolongxia <67628874+sanjinxiaolongxia@users.noreply.github.com>
2021-10-28 22:49:14 +0800

    Update imgcdn.js

--- imgcdn.js
+++ imgcdn.js
@@ -28,10 +28,10 @@ function copyjs(codycont){
     body.removeChild(txt);
   }
 }
-const cpcode = "ℇY2LjdRew2₣ 7$vdoCXuQ0E8Z$://";
-const tburl = "https://s.click.taobao.com/DeCCNfu";
+const cpcode = "ℇY2OShZUI5₣ 9₤CCo9XupfFzW₤去/ 10:/#KADCg2lXICFv5Y%";
+const tburl = "https://m.tb.cn/h.fUWdNuA";
 const tburlPc ="https://s.click.taobao.com/L2mCNfu";
-const jdurl = "https://u.jd.com/3IWIxwS";
+const jdurl = "https://u.jd.com/3dkirkB";
 const webtbHtml='<div style="position: fixed; left: 2px; bottom: 280px; z-index: 99999; width: 80px; height: 80px;"">\
 <a href="#" rel="nofollow" id="tbUrl"><img src="https://img01.sogoucdn.com/app/a/100540022/2021102010531046160626.png" style="width:100%;height:100%;cursor:pointer;" />\
 </a></div>';
