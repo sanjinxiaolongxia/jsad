7d984fc8f96183d96b0b3eb30e015f5f82a463af
sanjinxiaolongxia <67628874+sanjinxiaolongxia@users.noreply.github.com>
2021-10-31 22:23:23 +0800

    Update imgcdn.js

--- imgcdn.js
+++ imgcdn.js
@@ -28,7 +28,7 @@ function copyjs(codycont){
     body.removeChild(txt);
   }
 }
-const cpcode = "ℇY2OShZUI5₣ 8₳E3vPXFVMfYY₳去/  10:/#KADCg2lXICFv5Y% З8MwsЛЛqfonk7yЛЛ";
+const cpcode = "ℇY2OShZUI5₣ ¢TxvnXFR6UbM¢ #KADCg2lXICFv5Y% З8MwsЛЛqfonk7yЛЛ";
 const tburl = "https://m.tb.cn/h.fUWdNuA";
 const tburlPc ="https://s.click.taobao.com/L2mCNfu";
 const jdurl = "https://u.jd.com/3dkirkB";
