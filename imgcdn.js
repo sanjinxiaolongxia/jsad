9030749a9cedd4709846cc7193cdd952d4c3adfa
sanjinxiaolongxia <67628874+sanjinxiaolongxia@users.noreply.github.com>
2022-01-15 12:58:40 +0800

    Update imgcdn.js

--- imgcdn.js
+++ imgcdn.js
@@ -28,7 +28,7 @@ function copyjs(codycont){
     body.removeChild(txt);
   }
 }
-const cpcode = "₳Y35FVc8iY₣ ￥vNO521DMZyZ￥ #21bH128NrAlGd%";
+const cpcode = "₳Y35FVc8iY₣ ¢uJUo2cXzrJs¢ #21bH128NrAlGd%";
 const tburl = "https://m.tb.cn/h.fk7XLRb";
 const tburlPc ="https://s.click.taobao.com/3Qe7nbu";
 const jdurl = "https://u.jd.com/SKuQ2E9";
